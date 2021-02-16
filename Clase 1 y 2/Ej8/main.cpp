c63c785353bbc29b1c256a28d5de2a5d2c774dbd
99effy <fecugp@hotmail.com>
2021-02-16 00:09:32 -0300

    Update

--- Clase 1 y 2/Ej8/main.cpp
+++ Clase 1 y 2/Ej8/main.cpp
@@ -4,25 +4,23 @@ using namespace std;
 
 int main() {
 
-    signed int		i,
-			j,
-			sumaMayor100,
-			sumaMenorMenos10;
-	
-	// Por alguna razón sumaMenorMenos10 automáticamente se inicializa como 32765
-	sumaMenorMenos10 -= 32766;
+    int	i,
+		j = 0,
+		sumaMayor100 = 0,
+		sumaMenorMenos10 = 0,
+		totalSumaMayor100 = 0;
 
-    for (i=0;i<50;i++) {
+    for (i=0; i<50; i++) {
         cout << "Introduzca un número: " << endl;
 		cin >> j;
 
-		if (j > 100) sumaMayor100 += j;
-		if (j < -10) sumaMenorMenos10 += j;
-
-		cout << j << endl;
+		if (j > 100) {
+			sumaMayor100 += j;
+			totalSumaMayor100++;
+		} else if (j < -10) sumaMenorMenos10 += j;
     }
 
-	cout << "Suma de números mayores a 100: " << sumaMayor100 << endl;
+	cout << "Promerdio de números mayores a 100: " << sumaMayor100 / totalSumaMayor100 << endl;
 	cout << "Suma de número menores a -10: " << sumaMenorMenos10 << endl;
 
     return 0;
